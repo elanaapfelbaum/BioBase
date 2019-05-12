0e8310db9f972e28857409292a3010cd111201c8
Elana Apfelbaum <elana.apfelbaum@gmail.com>
2019-05-12 18:33:06 -0400

    updating sql script

--- public_html/dbsql.sql
+++ public_html/dbsql.sql
@@ -60,3 +60,17 @@ create table converts(
        foreign key(enzyme_name) references enzyme(enzyme_name),
        foreign key(reactant_name) references intermediate(intermediate_name),
        foreign key(product_name) references intermediate(intermediate_name));
+
+
+load data local infile 'enzymedata.txt' into table enzyme fields terminated by ' | ' lines terminated by '\n';
+load data local infile 'processdata.txt' into table process fields terminated by ' | ' lines terminated by '\n';
+load data local infile 'intermediatedata.txt' into table intermediate fields terminated by ' | ' lines terminated by '\n';
+load data local infile 'locationdata.txt' into table location fields terminated by ' | ' lines terminated by '\n';
+load data local infile 'conditiondata.txt' into table condition fields terminated by ' | ' lines terminated by '\n';
+
+
+load data local infile 'located_indata.txt' into table located_in fields terminated by ' | ' lines terminated by '\n';
+load data local infile 'convertsdata.txt' into table converts fields terminated by ' | ' lines terminated by '\n';
+load data local infile 'usesdata.txt' into table uses fields terminated by ' | ' lines terminated by '\n';
+load data local infile 'operates_underdata.txt' into table operates_under fields terminated by ' | ' lines terminated by '\n';
+
