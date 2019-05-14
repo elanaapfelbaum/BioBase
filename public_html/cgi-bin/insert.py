b3c7df0dc0ab6990406de5a65e7232fb13df0e5b
Elana Apfelbaum <elana.apfelbaum@gmail.com>
2019-05-13 22:10:51 -0400

    fixing error with insert

--- public_html/cgi-bin/insert.py
+++ public_html/cgi-bin/insert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3                                     
                                 
-import cgi, cgitb
+import cgi
 import mysql.connector
 
 form = cgi.FieldStorage()
@@ -10,13 +10,14 @@ values = form.getvalue('values').split(',')
 svalues = ""
 for value in values:
     # concatenate them into the appropriate syntax, removing any unnecessary whitespace
-    svalues += '"' + value.strip() + '", '
+    svalues += "'" + value.strip() + "', "
 svalues = svalues[:-2]
 
-cnx = mysql.connector.connect(user='eapfelba', host = 'localhost',
-                              database='eapfelba1', password='chumash1000')
-
+cnx = mysql.connector.connect(user='eapfelba', host = 'localhost', database='eapfelba1', password='chumash1000')
+query=""
 cursor = cnx.cursor()
+#query = ("select * from enzyme")
+#cursor.execute(query)
 
 # inserting into a table
 # need to also check if not the right form... right amount of attributes for that table
@@ -24,7 +25,7 @@ if insert_table and values:
     query = ("insert into " + insert_table + " values (" + svalues + ");")
 
 cursor.execute(query)
-#data = cursor.fetchall()
+
 
 # html with the response from the insert
 print("Content-type:text/html\r\n\r\n")
@@ -42,7 +43,8 @@ print("<center>")
 print("<h1>~BioBase~</h1>")
 print("<h2>Home of Biochemical Processes</h2>")
 print("<h3>Inserted " + svalues + " into " + insert_table + "!</h3>")
-print("<h3>with " + query + " error<h3>")
+print(query)
+#print("<h3>with "  + " error<h3>")
 print("</center>")
 print("</body>")
 print("</html>")
