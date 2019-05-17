46751c3f395d6a734043ea6d7a620a7af1e45a36
Elana Apfelbaum <elana.apfelbaum@gmail.com>
2019-05-16 22:42:02 -0400

    fixed update error and stripped whitespace

--- public_html/cgi-bin/delete.py
+++ public_html/cgi-bin/delete.py
@@ -4,29 +4,27 @@ import cgi
 import mysql.connector
 
 form = cgi.FieldStorage()
-enzyme_name = form.getvalue('enzyme_name')#.strip()
-process_name = form.getvalue('process_name')#.strip()
-enzyme_name2 = form.getvalue('enzyme_name2')#.strip()
-process_name2 = form.getvalue('process_name2')#.strip()
+enzyme_name = form.getvalue('enzyme_name')
+process_name = form.getvalue('process_name')
+enzyme_name2 = form.getvalue('enzyme_name2')
+process_name2 = form.getvalue('process_name2')
 enzyme_name3 = form.getvalue('enzyme_name3')
 
 cnx = mysql.connector.connect(user='eapfelba', database='eapfelba2', host='localhost', password='chumash1000')
 query = ""
 cursor = cnx.cursor()
-#query = "select * from enzyme"
-
 
 if enzyme_name:
-    query = ('delete from converts where enzyme_name = "' + enzyme_name + '"')
+    query = "delete from converts where enzyme_name = '" + enzyme_name.strip() + "'"
 
 if enzyme_name3:
-    query = ('delete from enzyme where enzyme_name = "' + enzyme_name3 + '"')
+    query = "delete from enzyme where enzyme_name = '" + enzyme_name3.strip() + "'"
     
 if process_name:
-    query = ('delete from process where process_name = "' + process_name + '"')
+    query = "delete from process where process_name = '" + process_name.strip() + "'"
 
 if process_name2 and enzyme_name2:
-    query = ('delete from uses where process_name = "' + process_name2 + '" and enzyme_name = "' + enzyme_name2 + '"')
+    query = "delete from uses where process_name = '" + process_name2.strip() + "' and enzyme_name = '" + enzyme_name2.strip() + "'"
 
 
 cursor.execute(query)
@@ -47,9 +45,7 @@ print("<body>")
 print("<center>")
 print("<h1>~BioBase~</h1>")
 print("<h2>Home of Biochemical Processes</h2>")
-print("<h3>Deleted!</h3>")
-#print(query)
-#print("<h3>with "  + " error<h3>")                                            
+print("<h3>Deleted!</h3>")                                      
 print("</center>")
 print("</body>")
 print("</html>")
--- public_html/cgi-bin/update.py
+++ public_html/cgi-bin/update.py
@@ -1,58 +1,54 @@
-#!/usr/bin/python3                                                                           
+#! /usr/bin/python3
 
 import cgi
 import mysql.connector
 
 form = cgi.FieldStorage()
-enzyme_name = form.getvalue('enzyme_name')
-product_name = form.getvalue('product_name')
-enzyme_name2 = form.getvalue('enzyme_name2')
+enzyme_name    = form.getvalue('enzyme_name')
+product_name   = form.getvalue('product_name')
+enzyme_name2   = form.getvalue('enzyme_name2')
 mechanism_name = form.getvalue('mechanism_name')
-process_name = form.getvalue('process_name')
-concentration = form.getvalue('concentration')
-compound_name = form.getvalue('compound_name')
+process_name   = form.getvalue('process_name')
+concentration  = form.getvalue('concentration')
+compound_name  = form.getvalue('compound_name')
+
 
 cnx = mysql.connector.connect(user='eapfelba', database='eapfelba2', host='localhost', password='chumash1000')
-cursor = cnx.cursor()
-query = "select * from enzyme"
+cursor = cnx.cursor(buffered=True)
 
 if enzyme_name and product_name:
-    query = ('update converts set product_name = "' + product_name + '" where enzyme_name = "' + enzyme_name + '"')
+    query = "update converts set product_name = '" + product_name.strip() + "' where enzyme_name = '" + enzyme_name.strip() + "'"
 
 if enzyme_name2 and mechanism_name:
-    query = ('update enzyme set mechanism = "' + mechanism_name + '" where enzyme_name = "' + enzyme_name2 + '"')
+    query = "update enzyme set ligand_mechanism = '" + mechanism_name.strip() + "' where enzyme_name = '" + enzyme_name2.strip() + "'"
 
-if process_name and concentration and compound_name:
-    query = ('update operates_under set concentration = "' + concentration + '" and compound_name = "' + compound_name + '" where process_name = "' + process_name + '"')
+if process_name and concentration and compound:
+    query = "update operates_under set concentration = '" + concentration.strip() + "', compound = '" + compound_name.strip() + "' where process_name = '" + process_name.strip() + "'"
 
 
 cursor.execute(query)
-#cnx.commit()
-#data = cursor.fetchall()
+cnx.commit()
 
-# html with the response from the update                   
+# html with the response from the update           
 print("Content-type:text/html\r\n\r\n")
 print("<html>")
-print(" <style>")
-print("body {")
-print("background-image: url(../biochem.png);")
-print("background-attachment: fixed;")
-print("background-size: cover;")
-print("background-position: center;")
-print("}>")
-print("</style>")
+print(" <style>")                                                  
+print("body {")                                   
+print("background-image: url(../biochem.png);")                        
+print("background-attachment: fixed;")                                 
+print("background-size: cover;")                                            
+print("background-position: center;")                          
+print("}>")                                                                        
+print("</style>")                   
 print("<body>")
 print("<center>")
 print("<h1>~BioBase~</h1>")
 print("<h2>Home of Biochemical Processes</h2>")
-print("<h3>Deleted!</h3>")
+print("<h3>Updated!</h3>")
 #print(query)
-#print(data)
-#print("<h3>with "  + " error<h3>") 
 print("</center>")
 print("</body>")
 print("</html>")
 
-#cursor.close()
-#cnx.close()
-
+cursor.close()
+cnx.close()
